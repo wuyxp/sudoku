4112b054bc01858397d4239408494f6df363a2f4
wuyang <wuyang@cloudin.cn>
2018-03-07 12:50:55 +0800

    增加创建矩阵，和随机洗牌算法

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 console.log('入口文件');
+
+// 返回行数组
+const makeRow = length => Array(length).fill(0).map((i, v) => v);
+
+// 返回矩阵数组
+const make = length => rowLength => Array(length).fill(0).map(i => makeRow(rowLength));
+
+const matrix = make(9)(9);
+console.log(matrix);
+
+// 洗牌算法
+const shuffle = arr => {
+  for(let i = 0,l = arr.length ; i < l ; i++){
+    let randomNum = Math.floor(i+(Math.random()*(l-i)));
+    [arr[i], arr[randomNum]] = [arr[randomNum], arr[i]];
+  }
+  return arr;
+}
+
+let arr = makeRow(9);
+console.log(arr);
+console.log(shuffle(arr));
