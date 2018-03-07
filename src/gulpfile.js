c333fb5d91c51383245fbfb33c75a16f186fb5d2
wuyang <wuyang@cloudin.cn>
2018-03-07 14:43:48 +0800

    增加样式框架，并且加入less自动编译

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,16 +2,25 @@ const gulp = require('gulp');
 
 const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config');
+
+const less = require('gulp-less');
 // 编译webpack
 gulp.task('webpack', () =>{
   return gulp.src('./js/index')
     .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest('../www/'))
+    .pipe(gulp.dest('../www/js/'))
 });
 
 // 编译less
 gulp.task('less', () => {
-
+  return gulp.src('./less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('../www/css/'))
 })
 
 gulp.task('default',['webpack', 'less']);
+
+gulp.task('watch', ()=>{
+  gulp.watch('./js/**/*.js',['webpack'])
+  gulp.watch('./less/**/*.less',['less'])
+})
---
+++ www/js/main.js
@@ -0,0 +1,111 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+console.log('入口文件');
+
+// 返回行数组
+var makeRow = function makeRow(length) {
+  return Array(length).fill(0).map(function (i, v) {
+    return v;
+  });
+};
+
+// 返回矩阵数组
+var make = function make(length) {
+  return function (rowLength) {
+    return Array(length).fill(0).map(function (i) {
+      return makeRow(rowLength);
+    });
+  };
+};
+
+var matrix = make(9)(9);
+console.log(matrix);
+
+// 洗牌算法
+var shuffle = function shuffle(arr) {
+  for (var i = 0, l = arr.length; i < l; i++) {
+    var randomNum = Math.floor(i + Math.random() * (l - i));
+    var _ref = [arr[randomNum], arr[i]];
+    arr[i] = _ref[0];
+    arr[randomNum] = _ref[1];
+  }
+  return arr;
+};
+
+var arr = makeRow(9);
+console.log(arr);
+console.log(shuffle(arr));
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map
